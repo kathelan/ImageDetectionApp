511128b31f41533899c7efddd5b64930670ab042
kathelan <mateuszdeptulski@gmail.com>
2021-03-28 20:45:48 +0200

    Initial commit

---
+++ src/main/java/pl/kathelan/kathelanimagedetection/KathelanImageDetectionApplication.java
@@ -0,0 +1,13 @@
+package pl.kathelan.kathelanimagedetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KathelanImageDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KathelanImageDetectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kathelan/kathelanimagedetection/KathelanImageDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kathelan.kathelanimagedetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KathelanImageDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
